0c3cf84257fbb8e7991f91daeb1c8758f30f9224
mkisser-tgm <mkisser@student.tgm.ac.at>
2020-01-22 13:26:54 +0100

    Fixed test examples

--- logicTests.py
+++ logicTests.py
@@ -8,3 +8,12 @@ def test_logic_init():
     from Controller import Controller
     controller = Controller()
     logic = GameLogic(controller)
+
+
+def test_generate_values():
+    from Controller import Controller
+    controller = Controller()
+    logic = GameLogic(controller)
+    logic.generate_mines(10,10)
+    assert len(logic.field_values) == 10
+    assert len(logic.field_values[0]) == 10
